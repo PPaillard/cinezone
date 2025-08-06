4c2161625da8436d9bae7663bb7199e3145f98ad
PierreP <paillard.pierre@gmail.com>
2025-08-06 13:39:36 +0200

    Index & show op

--- index.js
+++ index.js
@@ -9,6 +9,37 @@ const app = express();
 
 const serverPort = process.env.SERVER_PORT ?? 3000;
 
+app.get("/movies", function (req, res) {
+  database
+    .query("SELECT * FROM movies")
+    .then(([movies]) => {
+      res.json(movies);
+    })
+    .catch((error) => {
+      // serait bien de l'enregistrer dans un fichier de log
+      console.error(error);
+      res.sendStatus(500);
+    });
+});
+
+app.get("/movies/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  database
+    .query("SELECT * FROM movies WHERE id=?", [id])
+    .then(([movies]) => {
+      if (movies.length === 0) {
+        res.sendStatus(404);
+      } else {
+        res.json(movies[0]);
+      }
+    })
+    .catch((error) => {
+      // serait bien de l'enregistrer dans un fichier de log
+      console.error(error);
+      res.sendStatus(500);
+    });
+});
+
 // Pour verifier que l'api est opérationnelle
 app.get("/", (request, response) => {
   response.send("L'api fonctionne !");
